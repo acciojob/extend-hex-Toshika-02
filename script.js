6163cf07b4878c14172092cdbaf55669d9a9ad33
Toshika-02 <105552050+Toshika-02@users.noreply.github.com>
2024-01-15 17:03:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const extendHex = (shortHex) => {
   // write your code here
+	shortHex = shortHex.replace('#', '');
+    if (shortHex.length === 3) {
+    shortHex = shortHex[0] + shortHex[0] + shortHex[1] + shortHex[1] + shortHex[2] + shortHex[2];
+  }
+    return '#' + shortHex.toUpperCase();
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
+ const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
+ 
